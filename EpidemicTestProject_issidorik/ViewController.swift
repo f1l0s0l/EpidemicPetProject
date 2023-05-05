8b82cd219feb0aec5239f4d5d53879d069e9b516
Sidorik Ilia <iss19.spb@gmail.com>
2023-05-05 19:06:23 +0300

    Initial Commit

---
+++ EpidemicTestProject_issidorik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EpidemicTestProject_issidorik
+//
+//  Created by Илья Сидорик on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
